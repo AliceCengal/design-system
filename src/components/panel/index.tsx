7ff61b8477b9c958e029408818410fcd64edffb9
Athran Rahman <cengallut@gmail.com>
2023-12-24 20:57:17 +0800

    add spacing variables

--- src/components/panel/index.tsx
+++ src/components/panel/index.tsx
@@ -4,14 +4,31 @@ import styles from "./panel.module.css";
 export const panel = cva(styles.base, {
   variants: {
     kind: {
-      white: "",
-      back: "",
-      conBack: "",
+      white: styles.white,
+      back: styles.back,
+      conBack: styles.conBack,
       front: "",
       conFront: "",
+    },
+    maxWidth: {
+      auto: "",
+      xs: "",
+      sm: "",
+      md: "",
+      lg: "",
+    },
+    padding: {
+      0: "pad-0",
+      1: "pad-1",
+      2: "pad-2",
+      3: "pad-3",
+      4: "pad-4",
+      5: "pad-5",
     },
   },
   defaultVariants: {
     kind: "conBack",
+    maxWidth: "auto",
+    padding: 2,
   },
 });
