73be98204d930cbe6274d4ff6972544d581da6b7
Athran Rahman <cengallut@gmail.com>
2023-12-24 12:06:09 +0800

    add modal dialog component

---
+++ src/components/toggle-view/index.tsx
@@ -0,0 +1,28 @@
+import { JSX } from "preact/compat";
+import { useToggle } from "../../utils/use-toggle";
+import { MutableRef, useRef } from "preact/hooks";
+
+type ToggleViewProps = {
+  children: (open: boolean, toggleOpen: (a: any) => void) => JSX.Element;
+};
+
+export function ToggleView({ children }: ToggleViewProps) {
+  const [open, toggleOpen] = useToggle();
+
+  return children(open, toggleOpen);
+}
+
+type ToggleViewWithRefProps = {
+  children: (
+    open: boolean,
+    toggleOpen: (a: any) => void,
+    ref: MutableRef<any>
+  ) => JSX.Element;
+};
+
+export function ToggleViewWithRef({ children }: ToggleViewWithRefProps) {
+  const [open, toggleOpen] = useToggle();
+  const ref = useRef(null);
+
+  return children(open, toggleOpen, ref);
+}
