c0ba4a4c25e47ade4457992b4cef61ba67527712
Athran Rahman <cengallut@gmail.com>
2023-12-24 12:15:01 +0800

    change icons to outlined instead of filled

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,27 +1,35 @@
 export function SearchIcon() {
-  return <span className="material-icons">search</span>;
+  return <span className="material-icons-outlined">search</span>;
 }
 
 export function HomeIcon() {
-  return <span className="material-icons">home</span>;
+  return <span className="material-icons-outlined">home</span>;
 }
 
 export function ButtonIcon() {
-  return <span class="material-icons">indeterminate_check_box</span>;
+  return <span class="material-icons-outlined">indeterminate_check_box</span>;
 }
 
 export function CollapseIcon() {
-  return <span class="material-icons">keyboard_double_arrow_left</span>;
+  return (
+    <span class="material-icons-outlined">keyboard_double_arrow_left</span>
+  );
 }
 
 export function ExpandIcon() {
-  return <span class="material-icons">keyboard_double_arrow_right</span>;
+  return (
+    <span class="material-icons-outlined">keyboard_double_arrow_right</span>
+  );
 }
 
 export function OutboundIcon() {
-  return <span class="material-icons">open_in_new</span>;
+  return <span class="material-icons-outlined">open_in_new</span>;
 }
 
 export function ColorIcon() {
-  return <span class="material-icons">palette</span>;
+  return <span class="material-icons-outlined">palette</span>;
+}
+
+export function ChatIcon() {
+  return <span class="material-icons-outlined">chat_bubble_outline</span>;
 }
